aab1951ae9850185b829f3d0405b7665dc6fd7b0
Shukri Dozom <Shukridozom@hotmail.com>
2024-12-02 00:03:54 +0400

    Created a userModel schema

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IUser extends Document {
+    firstName: string,
+    lastName: String,
+    email: string,
+    passwordHash: string,
+    dateOfBirth: Date,
+}
+
+const userSchema: Schema<IUser> = new Schema({
+    firstName: {type: String, required: true},
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    passwordHash: {type: String, required: true},
+    dateOfBirth: {type: Date}
+});
+
+export const userModel = mongoose.model<IUser>("Users", userSchema);
