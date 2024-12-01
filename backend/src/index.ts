b780b1e4d54ddea6b0e69e7a50c52d5f7b141478
Shukri Dozom <Shukridozom@hotmail.com>
2024-12-02 00:03:49 +0400

    Created a docker-compose file to run a multi-container application for the backend and the database together

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 
-const DATABASE_HOST = "localhost";
-const DATABASE_PORT = 27017;
-const DATABASE_NAME = "e-commerce";
+if (!process.env.MONGO_URI) {
+    throw new Error('Environment variable MONGO_URL is not defined');
+  }
+const database_url = process.env.MONGO_URI;
 
 const app = express();
 const SERVER_HOST = "localhost";
 const SERVER_PORT = 8080;
 
 mongoose
-    .connect(`mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`)
+    .connect(database_url)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("Failed to connect to MongoDB"));
 
