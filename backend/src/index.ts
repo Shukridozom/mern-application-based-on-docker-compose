18d1b41053db67366f5431901e6188a98f00e7f5
Shukri Dozom <Shukridozom@hotmail.com>
2024-12-02 00:04:43 +0400

    Implemented a router for User (and route it to /users)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
 
 if (!process.env.MONGO_URI) {
     throw new Error('Environment variable MONGO_URL is not defined');
@@ -14,6 +15,11 @@ mongoose
     .connect(database_url)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("Failed to connect to MongoDB"));
+
+
+app.use(express.json());
+
+app.use('/user', userRoute);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server is running at: http://${SERVER_HOST}:${SERVER_PORT}`)
