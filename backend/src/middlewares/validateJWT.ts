16b2459233a01b660c85a78493e8f217018f6c4c
Shukridozom <Shukridozom@hotmail.com>
2024-12-03 08:33:31 +0400

    Fixed a but in validateJWT middleware

--- backend/src/middlewares/validateJWT.ts
+++ backend/src/middlewares/validateJWT.ts
@@ -11,24 +11,24 @@ export const validateJWT = (req: ExtendedRequest, res: Response, next: NextFunct
         const secretKey = '0a4f80a1e181f6345c783d66174da7980d43caadb5828b5637cd428bca47c4d4';
         const authHeader = req.get('authorization');
         if (!authHeader) {
-            res.status(401);
+            res.status(401).send();
             return;
         }
 
         const token = authHeader.split(" ")[1];
         if (!token) {
-            res.status(401);
+            res.status(401).send();
             return;
         }
 
         jwt.verify(token, secretKey, async (error, payload) => {
             if (error) {
-                res.status(401);
+                res.status(401).send();
                 return;
             }
 
             if (!payload) {
-                res.status(401);
+                res.status(401).send();
                 return;
             }
 
@@ -40,17 +40,16 @@ export const validateJWT = (req: ExtendedRequest, res: Response, next: NextFunct
 
             const user = await userModel.findOne({ email: userCredentials?.email })
             if (!user) {
-                res.status(401);
+                res.status(401).send();
                 return;
             }
-
             req.user = user;
 
             next();
         })
 
     } catch (error: any) {
-        res.status(500);
+        res.status(500).send();
         return;
     }
 }
