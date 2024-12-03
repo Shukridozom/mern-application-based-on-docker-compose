f4091be2c7e02d08ebb39b8c30ee7fe132e1d265
Shukridozom <Shukridozom@hotmail.com>
2024-12-03 06:36:23 +0400

    Created a Cart schema

---
+++ backend/src/models/cartModel.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IProduct } from "./productMode";
+
+const CartStatusEnum = ["active", "completed"]
+
+interface ICartItem extends Document {
+    product: IProduct,
+    unitPrice: number,
+    quantity: number
+}
+
+interface ICart extends Document {
+    userId: ObjectId | string,
+    items: ICartItem[],
+    totalAmount: number,
+    status: "active" | "completed"
+}
+
+const cartItemSchema: Schema<ICartItem> = new Schema({
+    product: { type: Schema.Types.ObjectId, ref: "Products", required: true },
+    unitPrice: { type: Number, required: true },
+    quantity: { type: Number, required: true, default: 1 }
+});
+
+const cartSchema: Schema<ICart> = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+    items: [cartItemSchema],
+    totalAmount: { type: Number, required: true },
+    status: { type: String, enum: CartStatusEnum, default: "active" }
+});
+
+export const cartModel = mongoose.model<ICart>("Carts", cartSchema);
--- backend/src/models/productMode.ts
+++ backend/src/models/productMode.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-interface IProduct extends Document {
+export interface IProduct extends Document {
     title: string,
     image: string,
     price: number,
@@ -8,10 +8,10 @@ interface IProduct extends Document {
 }
 
 const productSchema: Schema<IProduct> = new Schema({
-    title: {type: String, required: true},
-    image: { type: String, required: true},
-    price: { type: Number, required: true},
-    stock: {type: Number, required: true, default: 0},
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true, default: 0 },
 });
 
 export const productModel = mongoose.model<IProduct>("Products", productSchema);
