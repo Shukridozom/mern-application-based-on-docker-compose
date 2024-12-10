c5b18de9d55f1ab618cd04f254fb602ebe190209
Shukri Dozom <Shukridozom@hotmail.com>
2024-12-10 09:39:38 +0400

    Created a script for scaffolding a new react project

---
+++ frontend/scaffold-react-frontend-project.sh
@@ -0,0 +1,8 @@
+node -v > .nvmrc;
+npm create vite@latest frontend -y
+
+# -----------------------------------------------------------------------------------------------------------
+# Scaffold a new react project based on TypeScript using vite
+
+cd frontend
+npm i
