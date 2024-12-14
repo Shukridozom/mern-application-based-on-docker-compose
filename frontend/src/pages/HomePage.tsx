84e4924f058e1abb4ce63112e3a32cef05b25469
Shukri Dozom <Shukridozom@hotmail.com>
2024-12-14 20:57:01 +0400

    created a ProductCard component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,15 @@
+import { Container, Grid } from "@mui/material";
+import ProductCard from "../components/ProductCard";
+
 const HomePage = () => {
     return (
-        <>
-        <div>home page</div>
-        </>
+        <Container sx={{mt: 1}}>
+            <Grid container spacing={2}>
+            <Grid item md={4}><ProductCard /></Grid>
+            <Grid item md={4}><ProductCard /></Grid>
+            <Grid item md={4}><ProductCard /></Grid>
+            </Grid>
+        </Container>
     );
 }
 
